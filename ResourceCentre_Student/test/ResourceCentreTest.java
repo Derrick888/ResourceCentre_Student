b413413ce8a02c8b3419267db3315cb123eddffa
Derrick888 <22011505@myrp.edu.sg>
2023-07-13 15:08:33 +0800

    l09

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -255,7 +255,7 @@ public class ResourceCentreTest {
 	public void testDoReturnChromebook() {
 		//fail("Not yet implemented");
 		// write your code here
-		
+		//test
 		// Test case 1: Return a loaned out item
 		assertNotNull("Test if there is valid Chromebook arraylist to add to", chromebookList);
 		ResourceCentre.addChromebook(chromebookList, cb1);
