8c3c52064c04f70472e5a430631365bd2dc648c5
MedicalNerd <22021421@myrp.edu.sg>
2023-07-13 15:13:54 +0800

    heeee

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -268,7 +268,7 @@ public class ResourceCentre {
 			}
 		}
 		return isLoaned;
-	//dds
+	//dd
 	}
 	public static void loanChromebook(ArrayList<Chromebook> chromebookList) {
 		// write your code here		
